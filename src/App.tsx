5700a6e15ed486ca9bd973eac685cb634504dd9b
Abhishek Deshmukh <Abhishekdeshmukh74@gmail.com>
2020-08-29 21:38:20 +0530

    npm create app and added vscode debug configuration

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Abhishek Deshmukh';
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>My name is {name}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
